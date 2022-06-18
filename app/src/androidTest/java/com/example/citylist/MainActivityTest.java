51100c6e3dd4c80981b05c69b9ee11fefffa1268
tashin <ahamed1807083@stud.kuet.ac.bd>
2022-06-19 01:15:21 +0600

    UI_TEST

--- app/src/androidTest/java/com/example/citylist/MainActivityTest.java
+++ app/src/androidTest/java/com/example/citylist/MainActivityTest.java
@@ -75,5 +75,20 @@ public class MainActivityTest {
         onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click()); //Check the content on the list - no content in this case
         Espresso.pressBack(); //Back button
     }
+    @Test
+    public void UI_TEST(){
+        onView(withId(R.id.button_add)).perform(click()); //Click add button to add a city to the list
+        onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Gattia")); //Type a city name
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.button_confirm)).perform(click()); //Confirm the city name and add to the list
+
+        onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click()); //Check the content on the list - no content in this case
+        //Espresso.pressBack(); //Back button
+        onView(withId(R.id.second)).check(matches(isDisplayed()));
+        onView(withText("Gattia")).check(matches(isDisplayed()));
+        onView(withId(R.id.button)).perform(click());
+        onView(withId(R.id.main)).check(matches(isDisplayed()));
+    }
+
 
 }
